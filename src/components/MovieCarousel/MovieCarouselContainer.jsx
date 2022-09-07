33a05bf807ec3a91bdd831781be0f6c9ad303940
Juan Manuel Gutierrez Monaco <juan.monaco.gutierrez@gmail.com>
2022-09-07 20:20:38 -0300

    arreglos a carousel

--- src/components/MovieCarousel/MovieCarouselContainer.jsx
+++ src/components/MovieCarousel/MovieCarouselContainer.jsx
@@ -3,16 +3,16 @@ import MovieCarouselHeader from "./MovieCarouselHeader";
 
 const MovieCarouselContainer = () => {
     const slides = [
-        "https://image.tmdb.org/t/p/original//ugS5FVfCI3RV0ZwZtBV3HAV75OX.jpg",
-        "https://image.tmdb.org/t/p/original//xVbppM1xgbskOKgOuV8fbWBWHtt.jpg",
-        "https://image.tmdb.org/t/p/original//bqJXL8DRUIN6WsF86VB87pZZCOr.jpg",
-        "https://image.tmdb.org/t/p/original//8TUb2U9GN3PonbXAQ1FBcJ4XeXu.jpg",
-        "https://image.tmdb.org/t/p/original//92PJmMopfy64VYjd0HvIQaHGZX0.jpg",
+        {img: "https://image.tmdb.org/t/p/original//ugS5FVfCI3RV0ZwZtBV3HAV75OX.jpg", titulo: "Dragon Ball Super: Super Hero"},
+        {img: "https://image.tmdb.org/t/p/original//bqJXL8DRUIN6WsF86VB87pZZCOr.jpg", titulo: "30 noches con mi ex"},
+        {img: "https://image.tmdb.org/t/p/original//xVbppM1xgbskOKgOuV8fbWBWHtt.jpg", titulo: "¡Nop!"},
+        {img: "https://image.tmdb.org/t/p/original//8TUb2U9GN3PonbXAQ1FBcJ4XeXu.jpg", titulo: "Bestia"},
+        {img: "https://image.tmdb.org/t/p/original//92PJmMopfy64VYjd0HvIQaHGZX0.jpg", titulo: "El Perro Samurai: La Leyenda de Kakamucho"},
     ]
 
     return (
         <div className="movieCarousel-container">
-            <MovieCarousel slides={slides} controls header autoPlay={false} titulo={'Películas'}/>
+            <MovieCarousel slides={slides} controls header titulo={'Películas'}/>
         </div>
     )
 }
--- src/components/MovieCarousel/MovieCarouselItem.jsx
+++ src/components/MovieCarousel/MovieCarouselItem.jsx
@@ -1,7 +1,11 @@
 const MovieCarouselItem = ({ slide, stopSlide, startSlide }) => {
     return (
         <div className="movieCarousel-item" onMouseEnter={stopSlide} onMouseOut={startSlide}>
-            <img src={slide} alt="" />
+            <img src={slide.img} alt="" />
+            <div className="movieCarousel-item-desc">
+                <h3 className="uppercase">{slide.titulo}</h3>
+                <span className="uppercase">Más info</span>
+            </div>
         </div>
     )
 }
