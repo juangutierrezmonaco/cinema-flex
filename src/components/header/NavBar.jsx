b41798676c3ea1981ed4ecf9f5bff31fcda3ce46
Juan Manuel Gutierrez Monaco <juan.monaco.gutierrez@gmail.com>
2022-09-08 15:03:07 -0300

    Navbar responsive

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -15,25 +15,27 @@ const NavBar = () => {
 
     /* Mostrar qué sección está seleccionada */
     const navLinkClass = (navData) => (navData.isActive ? 'nav-active  hover:drop-shadow-mine ' : 'underline-hover hover:drop-shadow-mine ');  
+
+    const bntStyles = 'btn btn-circle min-h-0 h-8 w-8 text-sm sm:text-xl sm:h-10 sm:w-10 xl:text-2xl xl:h-12 xl:w-12';
     
     return (
-        <header className="myNavbar flex justify-between items-center py-5 px-10">
-            <div className='flex gap-3'> 
+        <header className="myNavbar flex justify-between items-center py-5 px-4 text-sm sm:text-xl sm:px-10 md:text-xl md:py-2 xl:text-2xl">
+            <div className='flex items-center gap-3'> 
 
-                <label className='btn btn-circle swap swap-rotate lg:hidden' onChange={toggleButton}>
+                <label className={`swap swap-rotate lg:hidden ${bntStyles}`} onChange={toggleButton}>
                     <input type="checkbox" checked={openNav} readOnly/>
-                    <i className='swap-off fill-current fa-solid fa-bars text-2xl'></i>
-                    <i className='swap-on fill-current fa-solid fa-xmark text-2xl'></i>
+                    <i className='swap-off fill-current fa-solid fa-bars'></i>
+                    <i className='swap-on fill-current fa-solid fa-xmark'></i>
                 </label>
 
-                <Link to={'/'} className="p-3 text-xl flex items-center gap-3 nav-logo" onClick={closeNav}>
-                    <i className="fa-solid fa-film"></i>
+                <Link to={'/'} className="p-3 flex items-center gap-3 nav-logo" onClick={closeNav}>
+                    <i className="fa-solid fa-film "></i>
                     <span>CINEMAFLEX</span>
                 </Link>
             </div>
 
             <nav>
-                <ul className="uppercase hidden lg:flex gap-5 text-xl whitespace-nowrap" >
+                <ul className="uppercase hidden lg:flex gap-5 whitespace-nowrap" >
                     <li><NavLink to={'/cartelera'} className={navLinkClass}>Películas en cartel</NavLink></li>
                     <li><NavLink to={'/estrenos'} className={navLinkClass}>Próximos estrenos</NavLink></li>
                     <li><NavLink to={'/combos'} className={navLinkClass}>Combos</NavLink></li>
@@ -43,7 +45,7 @@ const NavBar = () => {
 
             {/* Nota: El invisible es para que no se vea la animación en la carga de la página */}
             <nav className={openNav ? 'nav-overlay lg:hidden slide-in-left' : `nav-overlay lg:hidden slide-out-left invisible`}>
-                <ul className="uppercase flex flex-col lg:flex-row gap-5 text-xl whitespace-nowrap" >
+                <ul className="uppercase flex flex-col items-center lg:flex-row gap-5 whitespace-nowrap" >
                     <li><NavLink to={'/'} className={navLinkClass} onClick={closeNav}>Inicio</NavLink></li>
                     <li><NavLink to={'/cartelera'} className={navLinkClass} onClick={closeNav}>Películas en cartel</NavLink></li>
                     <li><NavLink to={'/estrenos'} className={navLinkClass} onClick={closeNav}>Próximos estrenos</NavLink></li>
@@ -52,10 +54,10 @@ const NavBar = () => {
                 </ul>
             </nav>
 
-            <ul className="flex items-center gap-5 text-2xl nav-icons">
-                <li onClick={closeNav}><button><i className="fa-regular fa-user"></i></button></li>
-                <li onClick={closeNav}><button><i className="fa-solid fa-magnifying-glass"></i></button></li>
-                <li onClick={closeNav}><CartWidget/></li>
+            <ul className="flex items-center gap-2 nav-icons">
+                <li className={bntStyles} onClick={closeNav}><button><i className="fa-regular fa-user"></i></button></li>
+                <li className={bntStyles} onClick={closeNav}><button><i className="fa-solid fa-magnifying-glass"></i></button></li>
+                <li className={bntStyles} onClick={closeNav}><CartWidget/></li>
             </ul>
         </header>
     )
